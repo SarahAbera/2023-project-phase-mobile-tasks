9152515c2725d4f0a11a466d00f5b5bce152345c
SarahAbera <sarayele77@gmail.com>
2023-08-03 16:42:54 +0300

    restructured the code

--- on-boarding/circular_counter/lib/main.dart
+++ on-boarding/circular_counter/lib/main.dart
@@ -2,7 +2,23 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'screen.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Circular counter app',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme:
+            ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 10, 2, 36)),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Circular counter'),
+    );
+  }
+}
--- on-boarding/circular_counter/lib/screen.dart
+++ on-boarding/circular_counter/lib/screen.dart
@@ -1,22 +1,4 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      // title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 10, 2, 36)),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Circular counter'),
-    );
-  }
-}
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -27,16 +9,17 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-int _counter = 0;
-    void _decrementCounter() {
-      setState(() {
-        _counter--;
-        if (_counter < 0) {
+  int _counter = 0;
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
+      if (_counter < 0) {
         _counter = 10;
       }
-      });
-    }
-     void _incrementCounter() {
+    });
+  }
+
+  void _incrementCounter() {
     setState(() {
       _counter++;
       if (_counter > 10) {
@@ -47,7 +30,6 @@ int _counter = 0;
 
   @override
   Widget build(BuildContext context) {
-   
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -57,17 +39,16 @@ int _counter = 0;
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-             const Text(
+            const Text(
               'You have pushed the button this many times:',
               style: TextStyle(
-              fontSize: 15,
-              fontWeight: FontWeight.bold,
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
               ),
             ),
             Text(
               '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-
+              style: const TextStyle(fontSize: 80, fontWeight: FontWeight.bold),
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -76,34 +57,36 @@ int _counter = 0;
                   height: 70,
                   width: 170,
                   child: ElevatedButton(
-                  onPressed: _decrementCounter,
-                  child: Text("Decrease"),
-                  style: ElevatedButton.styleFrom(
-                    textStyle: TextStyle(fontSize: 20),
-                    backgroundColor: const Color.fromARGB(255, 69, 65, 122),
-                    foregroundColor: Colors.white, 
+                    onPressed: _decrementCounter,
+                    child: Text("Decrease"),
+                    style: ElevatedButton.styleFrom(
+                      textStyle: TextStyle(fontSize: 20),
+                      backgroundColor: const Color.fromARGB(255, 69, 65, 122),
+                      foregroundColor: Colors.white,
+                    ),
                   ),
                 ),
+                SizedBox(
+                  width: 30,
                 ),
-                SizedBox(width: 30,),
                 SizedBox(
                   height: 70,
                   width: 170,
-                  child:ElevatedButton(  
-                  onPressed: _incrementCounter,
-                  child: Text("Increase"),
-                  style: ElevatedButton.styleFrom(
-                  textStyle: TextStyle(fontSize:20),
-                  backgroundColor: const Color.fromARGB(255, 69, 65, 122),
-                  foregroundColor: Colors.white, 
-                 ),
-                 ),
+                  child: ElevatedButton(
+                    onPressed: _incrementCounter,
+                    child: Text("Increase"),
+                    style: ElevatedButton.styleFrom(
+                      textStyle: TextStyle(fontSize: 20),
+                      backgroundColor: const Color.fromARGB(255, 69, 65, 122),
+                      foregroundColor: Colors.white,
+                    ),
+                  ),
                 ),
-                
               ],
             ),
           ],
         ),
       ),
-  );}
+    );
+  }
 }
