98005457a1c9221903252c809aabd0e42e78b7a0
SarahAbera <sarayele77@gmail.com>
2023-08-22 12:05:37 +0300

    local data source implementation

--- todo_clean_architecture/lib/features/todo/Data/dataSources/tasks_local_datasource.dart
+++ todo_clean_architecture/lib/features/todo/Data/dataSources/tasks_local_datasource.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:todo_clean_architecture/features/todo/Data/models/task_model.dart';
 import 'package:todo_clean_architecture/features/todo/Domain/entities/task.dart';
 
 abstract class TasksLocalDataSource {
@@ -5,4 +9,25 @@ abstract class TasksLocalDataSource {
   Future<void> cacheCurrentTodoList(List<Tasks> todoListToCache);
 }
 
+const CACHED_TASKS = "CACHED_TASKS";
 
+class TasksLocalDataSourceImpl implements TasksLocalDataSource {
+  final SharedPreferences sharedPreferences;
+
+  TasksLocalDataSourceImpl({required this.sharedPreferences});
+
+  @override
+  Future<void> cacheCurrentTodoList(List<Tasks> todoListToCache) async{
+    await sharedPreferences.setString(
+        CACHED_TASKS, jsonEncode(todoListToCache));
+  }
+
+  @override
+  Future<List<Tasks>> getAllTasks() async {
+    final jsonString = sharedPreferences.getString(CACHED_TASKS);
+    final decodedJson = json.decode(jsonString!);
+    return decodedJson
+        .map<TaskModel>((object) => TaskModel.fromJson(object))
+        .toList();
+  }
+}
---
+++ todo_clean_architecture/test/features/todo/Data/dataSources/local_dataSource_test.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/services.dart';
+import 'package:mockito/mockito.dart';
+import 'package:mockito/annotations.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:todo_clean_architecture/features/todo/Data/dataSources/tasks_local_datasource.dart';
+import 'package:todo_clean_architecture/features/todo/Data/models/task_model.dart';
+import 'package:todo_clean_architecture/features/todo/Domain/entities/task.dart';
+import 'dart:convert';
+import '../../../fixtures/fixture_reader.dart';
+import 'local_dataSource_test.mocks.dart';
+
+@GenerateNiceMocks([MockSpec<SharedPreferences>()])
+void main() {
+  late MockSharedPreferences mockSharedPreferences;
+  late TasksLocalDataSourceImpl tasksLocalDataSourceImpl;
+
+  setUp(() {
+    mockSharedPreferences = MockSharedPreferences();
+    tasksLocalDataSourceImpl =
+        TasksLocalDataSourceImpl(sharedPreferences: mockSharedPreferences);
+  });
+
+  final tTasks = [
+    TaskModel(
+        id: "1",
+        title: "test task",
+        description: "test description",
+        dueDate: DateTime(2019, 01, 01),
+        completed: true),
+    TaskModel(
+        id: "1",
+        title: "test task",
+        description: "test description",
+        dueDate: DateTime(2019, 01, 01),
+        completed: true)
+  ];
+
+  group("Get all tasks", () {
+    test("should return tasks list from sharedPreference", () async {
+      // arrange
+      when(mockSharedPreferences.getString(any))
+          .thenAnswer((_) => fixture("cached_task.json"));
+      // act
+      final result = await tasksLocalDataSourceImpl.getAllTasks();
+      // assert
+      verify(mockSharedPreferences.getString(CACHED_TASKS));
+      expect(result, tTasks);
+    });
+
+    // test("should call sharedpreferences to cache data", () async {
+    //   final tTask = Tasks(
+    //     id: "2",
+    //     title: "title",
+    //     description: "description",
+    //     dueDate: DateTime(2020, 09, 08),
+    //   );
+    //   // act
+    //   tasksLocalDataSourceImpl.cacheCurrentTodoList(tTasks);
+
+    //   // assert
+    //   verify(mockSharedPreferences.getString(
+    //     CACHED_TASKS, tTask,
+    //   ));
+    // });
+  });
+}
